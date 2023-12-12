55c5fdb424f1d16f008f050204cffe2a016ff1fc
Roberto Butti <roberto.butti@gmail.com>
2023-12-12 09:06:16 +0100

    Managing error from api

--- src/routes/space-info/+page.js
+++ src/routes/space-info/+page.js
@@ -1,5 +1,6 @@
 import { useStoryblokApi } from '@storyblok/svelte';
 import { useStoryblok } from '$lib/sblib';
+import { error } from '@sveltejs/kit';
 export const prerender = true;
 
 /** @type {import('./$types').PageLoad} */
@@ -8,10 +9,31 @@ export async function load() {
 
 	let storyblokApi = await useStoryblokApi();
 
-	const dataSpace = await storyblokApi.get('cdn/spaces/me');
+	let space = false;
+	let errorMessage = '';
+	let errorCode = -1;
+	try {
+		const dataSpace = await storyblokApi.get('cdn/spaces/me');
+		space = dataSpace.data.space;
+	} catch (e) {
+		space = false;
+		let eobj = {};
+		errorMessage = 'Error';
+		if (typeof e === 'string') {
+			eobj = JSON.parse(e);
+			errorMessage = eobj.message + '(' + eobj.status + ')' + eobj.response;
+			errorCode = eobj.status;
+		}
+	}
 
-	console.log(dataSpace.data);
+	if (space === false) {
+		throw error(404, {
+			message: errorMessage,
+			code: errorCode
+		});
+	}
+
 	return {
-		space: dataSpace.data.space
+		space: space
 	};
 }
