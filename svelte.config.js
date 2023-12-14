c66d6845113d011d42288223504ca8a19f473696
Roberto Butti <roberto.butti@gmail.com>
2023-12-14 21:19:04 +0100

    migration to SvelteKit 2

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 // 001 import the vitePreprocess
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
