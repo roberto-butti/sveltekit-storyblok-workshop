afc0184faad0c8ef37a3972ed8586b636f68ea8e
Roberto Butti <roberto.butti@gmail.com>
2024-07-29 20:03:15 +0200

    Adding server proxy config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import { defineConfig } from 'vite';
 import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
+	server: { proxy: {} },
 	plugins: [sveltekit(), basicSsl()]
 });
